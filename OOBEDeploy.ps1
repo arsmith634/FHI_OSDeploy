46017c52645a2a87606952a4dadcc74ae9de86bd
arsmith634 <61803515+arsmith634@users.noreply.github.com>
2022-03-01 23:09:08 -0500

    Add files via upload

---
+++ OOBEDeploy.ps1
@@ -0,0 +1,7 @@
+$Params = @{
+    AddNetFX3 = $true
+    RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
+    UpdateDrivers = $true
+    UpdateWindows = $true
+}
+Start-OOBEDeploy @Params
